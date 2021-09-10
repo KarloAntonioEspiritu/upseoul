64579968b5553f648c81dfb7c093e391e58ac230
Karlo Espiritu <karlo.espiritu@yahoo.com>
2021-09-11 01:31:38 +0800

    sheets test

--- api.php
+++ api.php
@@ -1,1 +1,4 @@
-api key = AIzaSyAyyVKo7HC_44rG2cvync3Q3An4zyHO39g
+api_key = AIzaSyAyyVKo7HC_44rG2cvync3Q3An4zyHO39g
+
+client_id = 123272213948-40ad1dcf0ie6dhjou9i1ihdjt75vflij.apps.googleusercontent.com
+client_secret = 7HJuvKsVzkuNvt05nwKO7Isg
--- sheet.php
+++ sheet.php
@@ -11,10 +11,34 @@ $client->setAuthConfig(__DIR__ . '/credentials.json');
 
 $service = new Google_Service_Sheets($client);
 
-$spreadsheetId = "1qDy6SQdnobbAHUzrYa3xT-A1Xlvj-LwmUCuBFyDc5mU"; //It is present in your URL
-$get_range = "Sheet!A1:D5";
+$spreadsheetId = "1fLf_YO1P-oIRiEYk3qHk_3_8lBFW2ZqOhPZTTTDHUO0"; //It is present in your URL
+$range = "Sheet1";
+// $get_range = "Sheet!A1:D5";
 
-$response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
-$values = $response->getValues();
+$params = [
+	'valueInputOption' => 'RAW'
+];
+
+$insert = [
+	'insertDataOptions' => 'INSERT_ROWS'
+];
+
+$values = [
+	['testing' , 'yowyowyow'],
+];
+
+$body = new Google_Service_Sheets_ValueRange([
+	'values' => $values
+]);
+
+$result = $service->spreadsheets_values->append(
+	$spreadsheetId, 
+	$range,
+	$body,
+	$params 
+);
+
+// $response = $service->spreadsheets_values->get($spreadsheetId, $get_range);
+// $values = $response->getValues();
 ?>
 
